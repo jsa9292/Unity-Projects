5f27971a6a438a0cefdb730249660fb16b9437be
jsa9292 <jsa9292@gmail.com>
2022-05-05 21:38:01 +0900

    ui initial change + loading bar for editor

--- Guestbook Downgrade/Assets/ProgressBar/Script/ProgressBar.cs
+++ Guestbook Downgrade/Assets/ProgressBar/Script/ProgressBar.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-
+[ExecuteInEditMode]
 public class ProgressBar : MonoBehaviour
 {
 
@@ -110,10 +110,10 @@ public class ProgressBar : MonoBehaviour
                 nextPlay = Time.time + RepeatRate;
                 audiosource.PlayOneShot(sound);
             }
+            float barVal = Mathf.Clamp(com.rfilkov.kinect.KinectManager.Instance.user_tracked_dur, 0, 2f);
+            if (barVal >= 2f) UI_anim_control.instance.LoadingUser();
+            UpdateValue(barVal / 2f * 100f);
         }
-        float barVal = Mathf.Clamp(com.rfilkov.kinect.KinectManager.Instance.user_tracked_dur, 0, 2f);
-        if (barVal >= 2f) UI_anim_control.instance.LoadingUser();
-        UpdateValue( barVal/ 2f * 100f);
     }
 
 }
